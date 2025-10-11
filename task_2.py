551b0a50085d762fa15ce91a7ccff5499dad5141
i3oi3ka <i3oi3ka@ukr.net>
2025-10-11 23:14:25 +0300

    Refactor unique_ip_address function to encapsulate logic and improve readability

--- task_2.py
+++ task_2.py
@@ -1,19 +1,22 @@
 import json
 
 
-remote_addrs = []
-with open("lms-stage-access.log", "rb") as file:
-    for line in file:
-        line = line.strip()
-        if line:
-            try:
-                log_entry = json.loads(line)
-                remote_address = log_entry.get("remote_addr")
-                if remote_address:
-                    remote_addrs.append(remote_address)
+def unique_ip_address(path):
+    remote_addrs = []
+    with open(path, "rb") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                try:
+                    log_entry = json.loads(line)
+                    remote_address = log_entry.get("remote_addr")
+                    if remote_address:
+                        remote_addrs.append(remote_address)
 
-            except json.JSONDecodeError:
-                print(f"Skipping malformed JSON line: {line[:60]}...")
+                except json.JSONDecodeError:
+                    print(f"Skipping malformed JSON line: {line[:60]}...")
+    return set(remote_addrs)
 
 
-print(remote_addrs[125])
+remote_ip_address = unique_ip_address("lms-stage-access.log")
+print(remote_ip_address)
