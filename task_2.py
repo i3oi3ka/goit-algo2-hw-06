5db18a6d7c041ca439b312ac56807e60c15f8e83
i3oi3ka <i3oi3ka@ukr.net>
2025-10-12 23:59:01 +0300

    Refactor unique_ip_address function to measure execution time and improve output clarity

--- task_2.py
+++ task_2.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from datasketch import HyperLogLog
 
 hll = HyperLogLog(p=14)
@@ -23,10 +24,18 @@ def unique_ip_address(path):
 
 remote_ip_address = unique_ip_address("lms-stage-access.log")
 
+set_ip = set()
+time_start = datetime.now()
+for data in remote_ip_address:
+    set_ip.add(data)
+time_result = datetime.now() - time_start
 
+
+print(f"Реальна кількість унікальних елементів: {len(set_ip)}, time: {time_result}")
+time_start = datetime.now()
 for data in remote_ip_address:
     hll.update(data.encode("utf-8"))
-
+time_result = datetime.now() - time_start
 # Оцінка кількості унікальних елементів
-print(f"Реальна кількість унікальних елементів: {len(set(remote_ip_address))}")
-print(f"Оцінена кількість унікальних елементів: {hll.count()}")
+
+print(f"Оцінена кількість унікальних елементів: {hll.count()}, time: {time_result}")
